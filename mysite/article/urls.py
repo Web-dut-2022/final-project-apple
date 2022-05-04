d74bf8a1f5de0ed4c826edfa75aa5ac2d8363eff
金田 <80387529@mail.dlut.edu.cn>
2022-05-04 21:04:03 +0800

    upload files

--- mysite/article/urls.py
+++ mysite/article/urls.py
@@ -7,5 +7,9 @@ urlpatterns=[
     path('article-column/',views.article_column, name="article_column"),
     path('article-list/',views.article_list, name="article_list"),
     path('del-article/',views.del_article, name="del_article"),
+    re_path('article-detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$',views.article_detail, name="article_detail"),
+    path('rename-column/',views.rename_article_column, name="rename_article_column"),
+    path('del-column/',views.del_article_column, name="del_article_column"),
+    path('article-post/',views.article_post, name="article_post"),
     path('redit-article/<int:article_id>',views.redit_article, name="redit_article"),
 ]
