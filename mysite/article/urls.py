f0096b4c498d03203d8b0cf1fc1d6a21b7407793
金田 <80387529@mail.dlut.edu.cn>
2022-05-05 16:23:00 +0800

    upload files

--- mysite/article/urls.py
+++ mysite/article/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,re_path
-from . import views
+from . import views,list_views
 
 app_name="article"
 
@@ -12,4 +12,6 @@ urlpatterns=[
     path('del-column/',views.del_article_column, name="del_article_column"),
     path('article-post/',views.article_post, name="article_post"),
     path('redit-article/<int:article_id>',views.redit_article, name="redit_article"),
+    path('list-article-titles/',list_views.article_titles, name="article_titles"),
+    path('article-content/<int:id>/<slug:slug>/', list_views.article_detail, name="article_content"),
 ]
